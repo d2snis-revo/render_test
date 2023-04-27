3715c1873f1c028f3efd21255198d834faf22562
d2snis-revo <130095931+d2snis-revo@users.noreply.github.com>
2023-04-27 16:34:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def index():
 
 @app.route('/callback', methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers['x-line-signature']
     body = request.get_data(as_text=True)
     try:
         handler.handle(body, signature)
