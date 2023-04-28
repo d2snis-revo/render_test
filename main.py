b8593c5456fffa93ec48b09ea304e1f5c19f7c1b
d2snis-revo <130095931+d2snis-revo@users.noreply.github.com>
2023-04-28 18:51:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import itertools
+import math
 import os
 
 from flask import Flask, abort, request
@@ -7,64 +9,64 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('cKtDec+oMDzR+76vJ8PoLAc9EAIMz5K8QEugP3899a/59RPJD0eRNQh71MFIC1UoKKYvUDGyNjNOgCPsSm7+zhdULh/yp0zdRva7y6lOqZ3C3FSLMRRFXEXN7l9Cr1nNQ2bFJLuL7QShVyQimnRilQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('fe0d85ead292f342d68123c9033c430a')
+line_bot_api = LineBotApi(os.environ["cKtDec+oMDzR+76vJ8PoLAc9EAIMz5K8QEugP3899a/59RPJD0eRNQh71MFIC1UoKKYvUDGyNjNOgCPsSm7+zhdULh/yp0zdRva7y6lOqZ3C3FSLMRRFXEXN7l9Cr1nNQ2bFJLuL7QShVyQimnRilQdB04t89/1O/w1cDnyilFU="])
+handler = WebhookHandler(os.environ["fe0d85ead292f342d68123c9033c430a"])
 
-players = []
+pair_count = 0
+pair_combinations = []
 
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-
-@app.route('/callback', methods=['POST'])
+@app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers['x-line-signature']
+    signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return 'OK'
+    return "OK"
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    global players
-    text = event.message.text
-    if text == '!r':
-        players = []
-        reply_text = '参加人数を入力してください。'
-    elif text.isdigit():
-        num_players = int(text)
-        players = [str(i) for i in range(1, num_players+1)]
-        reply_text = '参加人数を登録しました。'
-    elif text == '!s':
-        if len(players) == 0:
-            reply_text = '参加人数が登録されていません。'
+    global pair_count, pair_combinations
+    message = ""
+    if event.message.text == "!r":
+        pair_count = 0
+        pair_combinations = []
+        message = "参加人数を入力してください。"
+    elif event.message.text == "!s":
+        if pair_count > 0:
+            match_count = int(math.ceil(pair_count / 3))
+            for i, pair_combination in enumerate(pair_combinations):
+                if i % match_count == 0:
+                    message += "\n試合{}\n".format(int(i / match_count) + 1)
+                for j, pair in enumerate(pair_combination):
+                    court = j % 3 + 1
+                    message += "コート{}：{}と{}\n".format(court, pair[0] + 1, pair[1] + 1)
         else:
-            num_courts = len(players) // 12
-            if num_courts < 1:
-                reply_text = '参加人数が12人未満のため、組み合わせを作成できません。'
-            else:
-                reply_text = ''
-                for i in range(num_courts):
-                    reply_text += f'\nコート{i+1}:\n'
-                    court_players = players[i*12:(i+1)*12]
-                    for j in range(6):
-                        pair = court_players[j*2:(j+1)*2]
-                        reply_text += f'{pair[0]}-{pair[1]}\n'
-        players = []
+            message = "参加人数が入力されていません。"
+    elif event.message.text.isdigit():
+        X = int(event.message.text)
+        if X >= 12 and X <= 100:
+            pair_count = int(X / 2)
+            message = "ペア数：{}\n".format(pair_count)
+
+            pairs = list(itertools.combinations(range(X), 2))
+            pair_combinations = list(itertools.combinations(pairs, pair_count))
+            message += "組み合わせ数：{}\n\n".format(len(pair_combinations))
+
+            for i, pair_combination in enumerate(pair_combinations):
+                if i == 0:
+                    message += "最初のペア：\n"
+                message += "ペア{}：{}と{}\n".format(i + 1, pair_combination[0][0] + 1, pair_combination[0][1] + 1)
+            message += "\nペアが決定されました。試合を始める場合は、\"!s\"を入力してください。"
+        else:
+            message = "12以上100以下の整数を半角で入力してください。"
     else:
-        reply_text = '「!r」で参加人数を登録、「!s」で組み合わせを表示します。'
+        message = "正しい形式で整数を入力してください。"
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_text)
-    )
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
