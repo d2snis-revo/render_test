a377420a9f5f4303df1fae0b1ba08f699e5c0d8a
d2snis-revo <130095931+d2snis-revo@users.noreply.github.com>
2023-04-28 18:57:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ["cKtDec+oMDzR+76vJ8PoLAc9EAIMz5K8QEugP3899a/59RPJD0eRNQh71MFIC1UoKKYvUDGyNjNOgCPsSm7+zhdULh/yp0zdRva7y6lOqZ3C3FSLMRRFXEXN7l9Cr1nNQ2bFJLuL7QShVyQimnRilQdB04t89/1O/w1cDnyilFU="])
-handler = WebhookHandler(os.environ["fe0d85ead292f342d68123c9033c430a"])
+line_bot_api = LineBotApi("cKtDec+oMDzR+76vJ8PoLAc9EAIMz5K8QEugP3899a/59RPJD0eRNQh71MFIC1UoKKYvUDGyNjNOgCPsSm7+zhdULh/yp0zdRva7y6lOqZ3C3FSLMRRFXEXN7l9Cr1nNQ2bFJLuL7QShVyQimnRilQdB04t89/1O/w1cDnyilFU=")
+handler = WebhookHandler("fe0d85ead292f342d68123c9033c430a")
 
 pair_count = 0
 pair_combinations = []
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers["x-line-signature"]
     body = request.get_data(as_text=True)
     try:
         handler.handle(body, signature)
@@ -42,7 +42,7 @@ def handle_message(event):
                     message += "\n試合{}\n".format(int(i / match_count) + 1)
                 for j, pair in enumerate(pair_combination):
                     court = j % 3 + 1
-                    message += "コート{}：{}と{}\n".format(court, pair[0] + 1, pair[1] + 1)
+                    message += "コート{}:{}と{}\n".format(court, pair[0] + 1, pair[1] + 1)
         else:
             message = "参加人数が入力されていません。"
     elif event.message.text.isdigit():
@@ -58,7 +58,7 @@ def handle_message(event):
             for i, pair_combination in enumerate(pair_combinations):
                 if i == 0:
                     message += "最初のペア：\n"
-                message += "ペア{}：{}と{}\n".format(i + 1, pair_combination[0][0] + 1, pair_combination[0][1] + 1)
+                message += "ペア{}:{}と{}\n".format(i + 1, pair_combination[0][0] + 1, pair_combination[0][1] + 1)
             message += "\nペアが決定されました。試合を始める場合は、\"!s\"を入力してください。"
         else:
             message = "12以上100以下の整数を半角で入力してください。"
