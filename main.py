e6c2f77072ac89113cc0936fcef424af23a4213b
d2snis-revo <130095931+d2snis-revo@users.noreply.github.com>
2023-04-30 03:44:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-import math
 import os
+import random
 
 from flask import Flask, abort, request
 from linebot import LineBotApi, WebhookHandler
@@ -9,58 +8,72 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi("cKtDec+oMDzR+76vJ8PoLAc9EAIMz5K8QEugP3899a/59RPJD0eRNQh71MFIC1UoKKYvUDGyNjNOgCPsSm7+zhdULh/yp0zdRva7y6lOqZ3C3FSLMRRFXEXN7l9Cr1nNQ2bFJLuL7QShVyQimnRilQdB04t89/1O/w1cDnyilFU=")
-handler = WebhookHandler("fe0d85ead292f342d68123c9033c430a")
+line_bot_api = LineBotApi('cKtDec+oMDzR+76vJ8PoLAc9EAIMz5K8QEugP3899a/59RPJD0eRNQh71MFIC1UoKKYvUDGyNjNOgCPsSm7+zhdULh/yp0zdRva7y6lOqZ3C3FSLMRRFXEXN7l9Cr1nNQ2bFJLuL7QShVyQimnRilQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('fe0d85ead292f342d68123c9033c430a')
 
-@app.route("/callback", methods=["POST"])
+
+@app.route('/callback', methods=['POST'])
 def callback():
-    signature = request.headers["x-line-signature"]
+    signature = request.headers['x-line-signature']
     body = request.get_data(as_text=True)
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return "OK"
+    return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    global pair_count, pair_combinations
-    message = ""
+    global participant_list
 
+    #参加人数の登録
     if event.message.text == "!r":
-        pair_count = 0
-        pair_combinations = []
-        message = "参加人数を入力してください。"
+        message = TextSendMessage(text="参加人数を入力してください")
+        line_bot_api.reply_message(event.reply_token, message)
+
+        @handler.add(MessageEvent, message=TextMessage)
+        def handle_participant_number(event):
+            try:
+                n = int(event.message.text)
+            except ValueError:
+                message = TextSendMessage(text="参加人数は数字で入力してください")
+                line_bot_api.reply_message(event.reply_token, message)
+                return
+
+            participant_list = ['participant' + str(i) for i in range(1, n+1)]
+            message = TextSendMessage(text="参加者リストを更新しました")
+            line_bot_api.reply_message(event.reply_token, message)
+
+    #参加人数から組み合わせを計算
     elif event.message.text == "!s":
-        if pair_count > 0:
-            match_count = int(math.ceil(pair_count / 3))
-            for i, pair_combination in enumerate(pair_combinations):
-                if i % match_count == 0:
-                    message += "\n試合{}\n".format(int(i / match_count) + 1)
-                for j, pair in enumerate(pair_combination):
-                    court = j % 3 + 1
-                    message += "コート{}:{}と{}\n".format(court, pair[0] + 1, pair[1] + 1)
-        else:
-            message = "参加人数が入力されていません。"
-    elif event.message.text.isdigit():
-        X = int(event.message.text)
-        if 12 <= X and X <= 100:
-            pair_count = int(X / 2)
-            message = "ペア数：{}\n".format(pair_count)
+        num_pairs = int(len(participant_list) / 2)
+        if num_pairs < 6:
+            message = TextSendMessage(text="参加人数が少なすぎます")
+            line_bot_api.reply_message(event.reply_token, message)
+            return
 
-            pairs = list(itertools.combinations(range(X), 2))
-            pair_combinations = list(itertools.combinations(pairs, pair_count))
-            message += "組み合わせ数：{}\n\n".format(len(pair_combinations))
+        random_participants = random.sample(participant_list, 12)
 
-            for i, pair_combination in enumerate(pair_combinations):
-                if i == 0:
-                    message += "最初のペア：\n"
-                message += "ペア{}:{}と{}\n".format(i + 1, pair_combination[0][0] + 1, pair_combination[0][1] + 1)
-            message += "\nペアが決定されました。試合を始める場合は、\"!s\"を入力してください。"
+        random.shuffle(random_participants)
+        assignment_list = [(random_participants[i], random_participants[i+1]) for i in range(0, len(random_participants), 2)]
 
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+        message = ""
+        for i, court_assignments in enumerate(assignment_list):
+            message += "{}コート : {} と {}\n".format(i+1, court_assignments[0], court_assignments[1])
 
-if __name__ == "__main__":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=message)
+        )
+
+        assignment_list = []
+
+    else:
+        pass
+
+
+if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
